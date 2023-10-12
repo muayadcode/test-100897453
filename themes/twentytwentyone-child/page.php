beadf01b196fedad6357b934cbe1165d59a3854e
Muayad <moayad_nashwan@yahoo.com>
2023-10-12 09:37:22 -0400

    updated readme

---
+++ themes/twentytwentyone-child/page.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header();
+$hello_world= 'Hello World !';
+?>
+<h1><?php echo $hello_world; ?></h1>;
+<?php
+
+
+/* Start the Loop */
+while ( have_posts() ) :
+	the_post();
+	get_template_part( 'template-parts/content/content-page' );
+
+	// If comments are open or there is at least one comment, load up the comment template.
+	if ( comments_open() || get_comments_number() ) {
+		comments_template();
+	}
+endwhile; // End of the loop.
+
+get_footer();
